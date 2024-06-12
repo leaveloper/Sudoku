635d93504e00474e6491ad2d9d094af3e683b8b1
ljuarezz <leaveloper@gmail.com>
2024-06-12 15:39:29 -0300

    Se actualiza SudokuComponent
    - Los parámetros referentes a las filas y columnas se reciben como Inputs del componente
     - Se movió la inicialización de variables de filas y columnas a ngOnInit.
    - Si la grilla es de 1 x 1, se aplican los estilos que redondean las esquinas a la única grilla/celda.

--- src/app/components/sudoku/sudoku.component.ts
+++ src/app/components/sudoku/sudoku.component.ts
@@ -2,7 +2,6 @@ import { AfterViewInit, Component, ElementRef, Input, OnInit, Output, Renderer2 
 import { GridComponent } from '../grid/grid.component';
 import { CenterDirective } from '../../directives/center/center.directive';
 import { GridContentDirective } from '../../directives/grid-content/grid-content.directive';
-import { generateRange } from '../../utils'
 
 @Component({
   selector: 'sudoku',
@@ -11,9 +10,9 @@ import { generateRange } from '../../utils'
   templateUrl: './sudoku.component.html',
   styleUrl: './sudoku.component.scss'
 })
-export class SudokuComponent implements AfterViewInit {
-  rowLength: number;
-  colLength: number;
+export class SudokuComponent implements OnInit, AfterViewInit {
+  @Input() rows: number;
+  @Input() cols: number;
 
   outerRowGrid: number;
   outerColGrid: number;
@@ -23,29 +22,52 @@ export class SudokuComponent implements AfterViewInit {
   arr: number[][];
   possibleNumbers: number[];
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+  constructor(
+    private elementRef: ElementRef, 
+    private renderer: Renderer2) {
     this.arr = []
-    this.rowLength = 2;
-    this.colLength = 2;
+    this.rows = 1;
+    this.cols = 1;
 
-    const cells = this.rowLength * this.colLength;
-
-    this.outerRowGrid = cells !== this.rowLength ? cells / this.rowLength : cells;
-    this.outerColGrid = cells !== this.colLength ? cells / this.colLength : cells;
-    this.innerRowGrid = cells / this.outerRowGrid;
-    this.innerColGrid = cells / this.outerColGrid;
+    this.outerRowGrid = 1;
+    this.outerColGrid = 1;
+    this.innerRowGrid = 1;
+    this.innerColGrid = 1;
 
     this.possibleNumbers = [];
   }
 
+  ngOnInit(): void {
+    const cells = this.rows * this.cols;
+
+    this.outerRowGrid = cells !== this.rows ? cells / this.rows : cells;
+    this.outerColGrid = cells !== this.cols ? cells / this.cols : cells;
+    this.innerRowGrid = cells / this.outerRowGrid;
+    this.innerColGrid = cells / this.outerColGrid;
+  }
+
   ngAfterViewInit(): void {
+    let all: boolean = false;
+
+    if (this.outerRowGrid * this.outerColGrid === 1)
+      all = true;
+
     let prevNumber = 0;
     this.elementRef.nativeElement.querySelectorAll('.inner-container').forEach((e: HTMLElement, i: number, array: Array<HTMLElement>) => {
+      if (all) {
+        this.addClass(e, 'top-left-corner', 0);
+        this.addClass(e, 'top-right-corner', 0);
+        this.addClass(e, 'bottom-left-corner', 0);
+        this.addClass(e, 'bottom-right-corner', 0);
+
+        return;
+      }
+
       if (i > 0 && (i === 1 || i !== prevNumber + 1)) { // Salvo el 1, los siguientes elementos no son consecutivos. Cada elemento es el siguiente índice más uno
         prevNumber = i;
         let className = '';
         let index = 0;
-
+        
         switch (i) {
           case 1: 
             className = 'top-left-corner';
@@ -64,12 +86,15 @@ export class SudokuComponent implements AfterViewInit {
             index = (this.innerRowGrid * this.innerColGrid) - 1;
             break;
         }
-        
-        if (className.length > 0) {
-          this.renderer.addClass(e, className);
-          this.renderer.addClass(e.querySelectorAll('input')[index], className);
-        }
+
+        if (className.length > 0)
+          this.addClass(e, className, index);
       }
     })
+  }
+
+  addClass(e: HTMLElement, className: string, index: number) {
+    this.renderer.addClass(e, className);
+    this.renderer.addClass(e.querySelectorAll('input')[index], className);
   }
 }
