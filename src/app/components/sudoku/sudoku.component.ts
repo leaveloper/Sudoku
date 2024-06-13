41371417ef635a084a8de57f9a188e3eed334a79
ljuarezz <leaveloper@gmail.com>
2024-06-13 19:55:04 -0300

    Modificación de SudokuComponent
    - Se inyecta SudokuService.
     - Se genera un tablero con las dimensiones dadas desde su invocación mediante los inputs 'rows' y  'cols'.
    - Se recorren las celdas de la grilla para colocar el número correspondiente ubicado en el arreglo con los valores del sudoku.

--- src/app/components/sudoku/sudoku.component.ts
+++ src/app/components/sudoku/sudoku.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, ElementRef, Input, OnInit, Output, Renderer2 
 import { GridComponent } from '../grid/grid.component';
 import { CenterDirective } from '../../directives/center/center.directive';
 import { GridContentDirective } from '../../directives/grid-content/grid-content.directive';
+import { SudokuService } from '../../services/sudoku.service';
 
 @Component({
   selector: 'sudoku',
@@ -14,6 +15,8 @@ export class SudokuComponent implements OnInit, AfterViewInit {
   @Input() rows: number;
   @Input() cols: number;
 
+  cells: number;
+
   outerRowGrid: number;
   outerColGrid: number;
   innerRowGrid: number;
@@ -24,73 +27,126 @@ export class SudokuComponent implements OnInit, AfterViewInit {
 
   constructor(
     private elementRef: ElementRef, 
-    private renderer: Renderer2) {
+    private renderer: Renderer2,
+    private sudokuService: SudokuService) {
     this.arr = []
     this.rows = 1;
     this.cols = 1;
-
+    this.cells = 1;
+    
     this.outerRowGrid = 1;
     this.outerColGrid = 1;
     this.innerRowGrid = 1;
     this.innerColGrid = 1;
-
+    
     this.possibleNumbers = [];
   }
+    
+  ngOnInit(): void {      
+    this.cells = this.rows * this.cols;
+    this.outerRowGrid = this.cells !== this.rows ? this.cells / this.rows : this.cells;
+    this.outerColGrid = this.cells !== this.cols ? this.cells / this.cols : this.cells;
+    this.innerRowGrid = this.cells / this.outerRowGrid;
+    this.innerColGrid = this.cells / this.outerColGrid;
 
-  ngOnInit(): void {
-    const cells = this.rows * this.cols;
-
-    this.outerRowGrid = cells !== this.rows ? cells / this.rows : cells;
-    this.outerColGrid = cells !== this.cols ? cells / this.cols : cells;
-    this.innerRowGrid = cells / this.outerRowGrid;
-    this.innerColGrid = cells / this.outerColGrid;
+    this.sudokuService.setSize(this.rows, this.cols);
+    this.arr = this.sudokuService.generate();
   }
 
   ngAfterViewInit(): void {
     let all: boolean = false;
-
+    
     if (this.outerRowGrid * this.outerColGrid === 1)
       all = true;
+    
+    let prevNumber: number = 0;
+    let countIndex: number = 0;
 
-    let prevNumber = 0;
-    this.elementRef.nativeElement.querySelectorAll('.inner-container').forEach((e: HTMLElement, i: number, array: Array<HTMLElement>) => {
-      if (all) {
-        this.addClass(e, 'top-left-corner', 0);
-        this.addClass(e, 'top-right-corner', 0);
-        this.addClass(e, 'bottom-left-corner', 0);
-        this.addClass(e, 'bottom-right-corner', 0);
+    let cellRowIndex: number = 0;
+    let cellOuterColIndex: number;
+    let cellColIndex: number = 0;
 
-        return;
-      }
+    let topRightCornerIndex: number = this.cells / this.cols;
+    if (topRightCornerIndex === 1)
+      topRightCornerIndex = this.cells;
 
-      if (i > 0 && (i === 1 || i !== prevNumber + 1)) { // Salvo el 1, los siguientes elementos no son consecutivos. Cada elemento es el siguiente índice más uno
+    const bottomLetCornerIndex:number = this.getNumber((this.outerRowGrid * this.outerColGrid) - this.outerColGrid);
+
+    this.elementRef.nativeElement.querySelector('.container').querySelector('.container').querySelectorAll('.inner-container').forEach((e: HTMLElement, i: number, array: Array<HTMLElement>) => {      
+      if (i > 0 && (i === 1 || i !== prevNumber + 1)) { // Salvo el 1, los siguientes elementos no son consecutivos. Cada elemento es el siguiente índice más uno        
+        let inputIndex = 0;
+        countIndex++;
+
         prevNumber = i;
         let className = '';
-        let index = 0;
-        
+
         switch (i) {
           case 1: 
             className = 'top-left-corner';
-            index = 0;
+            inputIndex = 0;
             break;
-          case this.outerColGrid + (this.outerColGrid - 1):
+          case topRightCornerIndex + (i - countIndex):
             className = 'top-right-corner';
-            index = this.innerColGrid - 1;
+            inputIndex = this.innerColGrid - 1;
             break;
-          case (array.length - 1) - ((this.outerColGrid - 1) * 2): // Offset de 2
+          case bottomLetCornerIndex:
             className = 'bottom-left-corner';
-            index = (this.innerRowGrid * this.innerColGrid) - this.innerColGrid;
+            inputIndex = (this.innerRowGrid * this.innerColGrid) - this.innerColGrid;
             break;
           case array.length - 1:
             className = 'bottom-right-corner';
-            index = (this.innerRowGrid * this.innerColGrid) - 1;
+            inputIndex = (this.innerRowGrid * this.innerColGrid) - 1;
             break;
-        }
+        }        
 
         if (className.length > 0)
-          this.addClass(e, className, index);
+          this.addClass(e, className, inputIndex);
+
+        const prevCellColIndex = cellColIndex;
+        const prevCellRowIndex = cellRowIndex;
+
+        let counterCellColIndex = 0;
+
+        Array.from(e.querySelectorAll('input')).forEach((ei: HTMLInputElement) => {
+          ei.value = this.arr[cellRowIndex][cellColIndex].toString();
+
+          cellColIndex++;
+          counterCellColIndex++;
+
+          if (counterCellColIndex >= this.innerColGrid){
+            cellColIndex = prevCellColIndex;
+            counterCellColIndex = 0;
+            cellRowIndex++;
+          }
+        })
+
+        cellColIndex += this.innerColGrid;
+        
+        if (cellColIndex >= this.cells)
+          cellColIndex = 0;
+        else {
+            if (cellRowIndex >= this.innerRowGrid)
+              cellRowIndex = prevCellRowIndex;
+        }
+
+        if (all) {
+          this.addClass(e, 'top-left-corner', 0);
+          this.addClass(e, 'top-right-corner', 0);
+          this.addClass(e, 'bottom-left-corner', 0);
+          this.addClass(e, 'bottom-right-corner', 0);
+          return;
+        }
       }
     })
+  }
+
+  getNumber(count: number) {
+    let result = 1;
+    for (let i: number = 0; i < count; i++) {
+      result += 2;
+    }
+
+    return result;
   }
 
   addClass(e: HTMLElement, className: string, index: number) {
